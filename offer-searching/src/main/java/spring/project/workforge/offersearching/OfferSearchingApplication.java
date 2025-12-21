34b7bde03b1be282285c91627681dd0ac7332987
Michal Andrzejczak <michandrzejczakofficial@gmail.com>
2025-12-22 00:05:45 +0100

    offer-searching service: starting application

---
+++ offer-searching/src/main/java/spring/project/workforge/offersearching/OfferSearchingApplication.java
@@ -0,0 +1,13 @@
+package spring.project.workforge.offersearching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfferSearchingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfferSearchingApplication.class, args);
+    }
+
+}
---
+++ offer-searching/src/test/java/spring/project/workforge/offersearching/OfferSearchingApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.project.workforge.offersearching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfferSearchingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
